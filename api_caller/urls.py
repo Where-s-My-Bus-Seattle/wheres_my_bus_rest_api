a348c7527b13036e3a7936454f22a26eba98d59e
Stephen Koch <47271868+kochsj@users.noreply.github.com>
2020-02-13 12:37:21 -0800

    dot as view in urls

--- api_caller/urls.py
+++ api_caller/urls.py
@@ -3,5 +3,5 @@ from .views import get_a_routes_closest_stop_and_arrival_time, show_me_the_reque
 
 urlpatterns=[
     path('v1/<lat>/<lon>/<bus_route>', get_a_routes_closest_stop_and_arrival_time, name='bus_data'),
-    path('v1/<lat>/<lon>', show_me_the_request, name='print')
+    path('v1/<lat>/<lon>', show_me_the_request.as_view(), name='print')
 ]
