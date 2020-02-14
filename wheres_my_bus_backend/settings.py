c5cefa13021a417a4f922450ab070aa935547ac7
glasscharlie <55407156+glasscharlie@users.noreply.github.com>
2020-02-13 18:22:38 -0800

    Update settings.py

--- wheres_my_bus_backend/settings.py
+++ wheres_my_bus_backend/settings.py
@@ -145,14 +145,7 @@ STATICFILES_DIRS = [
 ]
 
 REST_FRAMEWORK = {
-   'DEFAULT_PERMISSION_CLASSES': [
-       'rest_framework.permissions.IsAuthenticated',
-   ],
-   'DEFAULT_AUTHENTICATION_CLASSES': [
-       'rest_framework_simplejwt.authentication.JWTAuthentication',
-       'rest_framework.authentication.SessionAuthentication',
-       'rest_framework.authentication.TokenAuthentication',
-       'rest_framework.authentication.BasicAuthentication',
-   ],
+   'DEFAULT_PERMISSION_CLASSES': [],
+   'DEFAULT_AUTHENTICATION_CLASSES': [],
 }
 
