b0305d7380edb0865d5836dc817a7ee94517a1af
Stephen Koch <47271868+kochsj@users.noreply.github.com>
2020-02-24 14:11:43 -0800

    added status to JsonResponse.

--- wheres_my_bus_backend/settings.py
+++ wheres_my_bus_backend/settings.py
@@ -26,6 +26,7 @@ SECRET_KEY = env('SECRET_KEY')
  
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG')
+# ALLOWED_HOSTS = ['0.0.0.0']
 # ALLOWED_HOSTS = tuple(env.list('ALLOWED_HOSTS'))
 
 
