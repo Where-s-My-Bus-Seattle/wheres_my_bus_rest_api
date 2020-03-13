fc50227b53833efd25687c1d8eba76c4cdb6271f
Stephen Koch <47271868+kochsj@users.noreply.github.com>
2020-03-13 16:54:17 -0700

    test static audio file'

--- api_caller/views.py
+++ api_caller/views.py
@@ -31,14 +31,16 @@ class show_me_the_request(APIView):
         
         print('is bytes?: ', isinstance(the_audio_file, bytes))
 
-        a = ' '.join(format(x, 'b') for x in bytearray(the_audio_file))
+        # a = ' '.join(format(x, 'b') for x in bytearray(the_audio_file))
 
-        with open('bus_routes/audio.wav', 'wb') as audio:
-            audio.write(a)
+        # with open('bus_routes/audio.wav', 'wb') as audio:
+        #     audio.write(a)
+
+        # with open('Test_Audio.wav') as audio:
         
         # use the audio file as the audio source
         r = sr.Recognizer()
-        with sr.AudioFile(audio) as source: #### Assertion Error: given audio file must be a file name string or a file-like object
+        with sr.AudioFile("Test_Audio.wav") as source: #### Assertion Error: given audio file must be a file name string or a file-like object
             sa = r.record(source)  # read the entire audio file
 
         # recognize speech using Sphinx
